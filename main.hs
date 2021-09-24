2abaddc4e704555309fd51268c689a742e61fccc
DeltaRig <daniprigoli@gmail.com>
2021-09-24 18:47:06 -0300

    foldr

--- main.hs
+++ main.hs
@@ -16,4 +16,30 @@ signum' n = if n < 0 then -1 else if n == 0 then 0 else 1
 --             | n > 0 = 1
 --             | otherwise = 0
 
+sum' :: [Int] -> Int
+sum' (x : xs) = x + sum xs
+-- sum' [1,2,3]
+-- 1 + sum' [2,3]
+-- 1 + (2 + sum' [3])
+-- 1 + (2 + (3  + sum[]))
+-- 1 + (2 + (3  + 0))
+
+sum'' :: [Int] -> Int
+sum'' = foldr (+) 0
+-- sum'' [1,2,3]
+-- foldr (+) 0 [1,2,3]
+-- 1 : 2 : 3 : []
+-- 1 + 2 + 3 + 0
+
+prod' :: [Int] -> Int
+prod' = foldr (*) 1
+
+foldr' :: (a -> b -> b) -> b -> [a] -> b
+foldr' f v [] = v
+foldr' f v (x:xs) = f x (foldr' f v xs)
+-- é o operador (+, and, or...)
+
+length' :: [Int] -> Int
+length' = foldr (\_ n -> 1 + n) 0
+
 
