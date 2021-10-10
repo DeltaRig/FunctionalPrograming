9b5ca4e53a5b0c4c4f1ebc991d62fbdb87915f00
DeltaRig <daniprigoli@gmail.com>
2021-10-10 10:24:34 -0300

    estudo de tipos

--- exerciciosTipos.hs
+++ exerciciosTipos.hs
@@ -6,7 +6,7 @@ mult (m,n) = m*n
 copy :: a -> Pair a
 copy x = (x,x) 
 
-data Boolean = True | False
+-- data Boolean = True | False
 
 data Answer = Yes | No | Unknown
 
@@ -49,20 +49,45 @@ mult'' m n = int2nat (nat2int m * nat2int n)
 
 
 -- 2. O arquivo de prelúdio de Haskell define o tipo
-----data Ordering = LT | EQ | GT
+data Ordering = LT | EQ | GT
 -- juntamente com a função
 ----compare :: Ord a => a -> a -> Ordering
 -- Que decide se o primeiro parâmetro é um tipo ordenado menor que (LT), igual (EQ) ou maior que
 -- (GT) que o segundo. Usando esta função, redefina
 -- a função occurs :: Ord a => a -> Tree a -> Bool
 -- para árvores binárias de busca.
+occurs :: Ord a => a -> Tree a -> Bool
+occurs x (Leaf y) = (compare x y) == (compare 1 1)
+occurs x (Node l y r) = (compare x y) == (compare 1 1) || occurs x l || occurs x r
+
+
+data Tree a = Leaf a | Node (Tree a) a (Tree a)
+
+t0 :: Tree Int
+t0 = Node (Leaf 1) 5 (Leaf 6)
+
+t00 :: Tree Int
+t00 = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
 
 -- 3. Considere a seguinte definição de árvores binárias:
-----data Tree a = Leaf a | Node (Tree a) (Tree a)
+-- árvore binaria apenas com elementos nas folhas
+data TreeWithoutEN a = LeafWEN a | NodeWE (TreeWithoutEN a) (TreeWithoutEN a)
+
+tr6 :: TreeWithoutEN Int
+tr6 = NodeWE (NodeWE (LeafWEN 1) (LeafWEN 4)) (NodeWE (LeafWEN 6) (LeafWEN 9))
+
+
 -- Dizemos que uma árvore binária é balanceada
 -- quando o número de folhas na subárvore à esquerda difere em no máximo 1 do número de folhas da subárvore à 
 -- direita. Defina uma função
--- balanced :: Tree a -> Bool que decide se uma árvore é balanceada ou não.
+--balanced :: Tree a -> Bool que decide se uma árvore é balanceada ou não.
+treeSize :: Tree a -> Int
+treeSize (Leaf x) = 1
+treeSize (Node l x r) = 1 + treeSize r + treeSize l
+
+balanced :: Tree a -> Bool
+balanced (Leaf _) = True
+balanced (Node l x r) = abs (treeSize l - treeSize r) <= 1 && balanced l && balanced r
 
 -- 4. Defina a função balance :: [a] -> Tree a que
 -- converte uma lista não-vazia em uma árvore balanceada.
@@ -70,13 +95,53 @@ mult'' m n = int2nat (nat2int m * nat2int n)
 --metadas cujo tamanho difere em no máximo 1.
 --Dica 2: você já fez esta função auxiliar.
 
+halve :: [a] -> ([a], [a])
+halve xs = splitAt (length xs `div` 2) xs
+
+-- recebe uma lista e transforma em uma arvore balanceada sem elementos nos nodos
+balance' :: [a] -> TreeWithoutEN a
+balance' [x] = LeafWEN x
+balance' xs = NodeWE (balance' ys)(balance' zs)
+            where (ys, zs) = halve xs
+
+middle []    = []
+middle [x]   = [x]
+middle [x,y] = [x,y]
+middle xs    = middle $ init $ tail xs
+
+remove :: Eq a => a -> [a] -> [a]
+remove element list = filter (\e -> e/=element) list
+
+-- meu problema nesse exercicio é que os nodos e as folhas tem que ser do mesmo tipo, mas eu tenho uma lista que vai virar In
+-- para gerar árvores binaria com elementos nos nodos e nas folhas a partir de listas
+--balance' :: [a] -> Tree a
+--balance' [x] = Leaf x
+--balance' (xs) = Node (balance' ys) (xs!!((length xs `div` 2)-1)) (balance' zs)
+--            where
+--            (ys, zs) = halve ([0..(length xs `div` 2)-1] ++ [(length xs `div` 2)+1..(length xs -1)])
+
+
+-- 4.2 Receber uma árvore a achatar ela numa lista
+flatten :: Tree a -> [a]
+flatten (Leaf x) = [x]
+flatten (Node l x r) = flatten l ++ [x] ++ flatten r
+
+t1 :: Tree Int
+t1 = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+-- Passo a passo
+-- > flatter t
+-- [1,3,4,5,6,7,9]
+
+
 --5. Dada a seguinte definição de tipo
-----data Expr = Val Int | Add Expr Expr
+data Expr = Val Int | Add Expr Expr
 --defina a função de alta-ordem
 ----folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
 --tal que folde f g substitui o construtor Val em
 --uma expressão pela função f assim como todo
 --construtor add pela função g.
+--folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
+--folde f g = 
 
 --6. Usando a função folde, defina a função
 ----eval :: Expr -> Int que avalia o valor de uma
